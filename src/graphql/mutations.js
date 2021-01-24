13c836580adb33719a4c0820f4659680c05535b4
Hannes Gebker <hannes.gebker@uni-muenster.de>
2021-01-24 14:19:14 +0100

    #27 Add amplify and appsync

---
+++ src/graphql/mutations.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createEvening = /* GraphQL */ `
+  mutation CreateEvening($input: CreateEveningInput!) {
+    createEvening(input: $input) {
+      Datum
+      hannes
+      louisa
+      tim
+      sonstige
+      ole
+      jan
+      semester
+    }
+  }
+`;
+export const updateEvening = /* GraphQL */ `
+  mutation UpdateEvening($input: UpdateEveningInput!) {
+    updateEvening(input: $input) {
+      Datum
+      hannes
+      louisa
+      tim
+      sonstige
+      ole
+      jan
+      semester
+    }
+  }
+`;
+export const deleteEvening = /* GraphQL */ `
+  mutation DeleteEvening($input: DeleteEveningInput!) {
+    deleteEvening(input: $input) {
+      Datum
+      hannes
+      louisa
+      tim
+      sonstige
+      ole
+      jan
+      semester
+    }
+  }
+`;
