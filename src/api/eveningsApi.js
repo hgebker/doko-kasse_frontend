97741d45523b9acd4188b03385b000d8ee8a3cf3
Hannes Gebker <hannes.gebker@uni-muenster.de>
2021-01-28 22:43:14 +0100

    Add authmode

--- src/api/eveningsApi.js
+++ src/api/eveningsApi.js
@@ -5,7 +5,11 @@ import { apiUtils } from 'services/utils';
 
 const listEvenings = async filter => {
   try {
-    const response = await API.graphql({ query: queries.listEvenings, variables: { limit: 100, filter } });
+    const response = await API.graphql({
+      query: queries.listEvenings,
+      variables: { limit: 100, filter },
+      authMode: 'API_KEY'
+    });
 
     return response.data.listEvenings.items;
   } catch (error) {
@@ -16,7 +20,11 @@ const listEvenings = async filter => {
 
 const getEvening = async date => {
   try {
-    const response = await API.graphql({ query: queries.getEvening, variables: { Datum: date } });
+    const response = await API.graphql({
+      query: queries.getEvening,
+      variables: { Datum: date },
+      authMode: 'API_KEY'
+    });
 
     return response.data.listEvenings.items;
   } catch (error) {
@@ -27,7 +35,11 @@ const getEvening = async date => {
 
 const createEvening = async newEvening => {
   try {
-    await API.graphql({ query: mutations.createEvening, variables: { input: newEvening } });
+    await API.graphql({
+      query: mutations.createEvening,
+      variables: { input: newEvening },
+      authMode: 'API_KEY'
+    });
     return newEvening;
   } catch (error) {
     apiUtils.logError(error);
@@ -37,7 +49,11 @@ const createEvening = async newEvening => {
 
 const updateEvening = async eveningToUpdate => {
   try {
-    await API.graphql({ query: mutations.updateEvening, variables: { input: eveningToUpdate } });
+    await API.graphql({
+      query: mutations.updateEvening,
+      variables: { input: eveningToUpdate },
+      authMode: 'API_KEY'
+    });
     return eveningToUpdate;
   } catch (error) {
     apiUtils.logError(error);
@@ -47,7 +63,11 @@ const updateEvening = async eveningToUpdate => {
 
 const deleteEvening = async date => {
   try {
-    await API.graphql({ query: mutations.deleteEvening, variables: { input: { Datum: date } } });
+    await API.graphql({
+      query: mutations.deleteEvening,
+      variables: { input: { Datum: date } },
+      authMode: 'API_KEY'
+    });
   } catch (error) {
     apiUtils.logError(error);
     throw error;
