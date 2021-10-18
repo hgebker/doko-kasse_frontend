06b8c60dce0ce7f4332a1d84cfc556ad6879db3c
Hannes Gebker <hannes.gebker@uni-muenster.de>
2021-10-18 22:27:59 +0200

    Add new semester

--- src/constants/semester.js
+++ src/constants/semester.js
@@ -22,6 +22,10 @@ const LIST_OPTIONS = [
   {
     id: 'ss21',
     label: 'Sommersemester 21'
+  },
+  {
+    id: 'ws2122',
+    label: 'Wintersemester 21/22'
   }
 ];
 
@@ -39,7 +43,8 @@ const SEMESTER_LABEL = {
   ws1920: 'Wintersemester 19/20',
   ss20: 'Sommersemester 20',
   ws2021: 'Wintersemester 20/21',
-  ss21: 'Sommersemester 21'
+  ss21: 'Sommersemester 21',
+  ws2122: 'Wintersemester 21/22'
 };
 
 export { LIST_OPTIONS, SEMESTER_OPTIONS, SEMESTER_LABEL };
